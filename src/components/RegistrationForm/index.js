6fe827b02ad0ba6138cd28d237fabbe4bcc059cb
Nadyan <nadyan_sp@hotmail.com>
2020-12-21 22:19:30 -0300

    field validations

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -19,6 +19,22 @@ export default function RegistrationForm() {
     const [socialId, setSocialId] = useState('');
     const [hints, setHints] = useState(false);
     const [newsletter, setNewsLetter] = useState(false);
+    const [error, setError] = useState(
+        {
+            socialId: {
+                valid: true,
+                text: ""
+            },
+            firstName: {
+                valid: true,
+                text: ""
+            },
+            lastName: {
+                valid: true,
+                text: ""
+            }
+        }
+    );
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -29,6 +45,83 @@ export default function RegistrationForm() {
             hints,
             newsletter
         };
+    }
+
+    function handleValidate(value, id) {
+        
+        let valid = true;
+        let text = '';
+
+        if (id === 'field-social-id') {
+
+            if (value.length !== 11) {
+                valid = false;
+                text = 'Social ID must have 11 digits'
+            }
+            /* TODO: Social Id validation funtion
+            else if (!isValidSocialId(value)) {
+                valid = false;
+                text = 'Social ID number is invalid'
+            }
+            */
+            
+            setError({
+                socialId: {
+                    valid: valid,
+                    text: text
+                },
+                firstName: {
+                    valid: error.firstName.valid,
+                    text: error.firstName.text
+                },
+                lastName: {
+                    valid: error.lastName.valid,
+                    text: error.lastName.text
+                }
+            });
+        } else if (id === 'field-first-name') {
+            
+            if (value.length === 0) {
+                valid = false;
+                text ="Last name can't be blank"
+            }
+            
+            setError({
+                socialId: {
+                    valid: error.socialId.valid,
+                    text: error.socialId.text
+                },
+                firstName: {
+                    valid: valid,
+                    text: text
+                },
+                lastName: {
+                    valid: error.lastName.valid,
+                    text: error.lastName.text
+                }
+            });
+        } else if (id === 'field-last-name') {
+            
+            if (value.length === 0) {
+                valid = false;
+                text ="Last name can't be blank"
+            }
+
+            setError({
+                socialId: {
+                    valid: error.socialId.valid,
+                    text: error.socialId.text
+                },
+                firstName: {
+                    valid: error.firstName.valid,
+                    text: error.firstName.text
+                },
+                lastName: {
+                    valid: valid,
+                    text: text
+                }
+            });
+        }
     }
 
     return (
@@ -46,6 +139,9 @@ export default function RegistrationForm() {
                     margin="dense"
                     value={firstName}
                     onChange={e => {setFirstName(e.target.value)}}
+                    error={!error.firstName.valid}
+                    helperText={error.firstName.text}
+                    onBlur={e => {handleValidate(e.target.value, e.target.id)}}
                 />
 
                 <TextField 
@@ -57,6 +153,9 @@ export default function RegistrationForm() {
                     margin="dense"
                     value={lastName}
                     onChange={e => {setLastName(e.target.value)}}
+                    error={!error.lastName.valid}
+                    helperText={error.lastName.text}
+                    onBlur={e => {handleValidate(e.target.value, e.target.id)}}
                 />
 
                 <TextField 
@@ -68,6 +167,9 @@ export default function RegistrationForm() {
                     margin="dense"
                     value={socialId}
                     onChange={e => {setSocialId(e.target.value)}}
+                    error={!error.socialId.valid}
+                    helperText={error.socialId.text}
+                    onBlur={e => {handleValidate(e.target.value, e.target.id)}}
                 />
 
                 <FormControlLabel
